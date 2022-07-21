db63751ec466ac7254567098f8d57902592eb292
stanford kangethe <stanfordkangethe16@gmail.com>
2022-07-21 20:01:28 +0300

    crud lab completed

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,13 +1,38 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion, onUpdateQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
-
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
     </option>
   ));
+
+  function handleDelete() {
+    onDeleteQuestion(id)
+  }
+
+  function handleChange() {
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({correctIndex}),
+    })
+      .then((response) => response.json())
+      .then((updatedQuestion) => {
+        const updatedQuestions = question.map((query) => {
+          if (query.id === updatedQuestion.id){
+            return updatedQuestion
+          } else {
+            return query
+          }
+        })
+        onUpdateQuestion(updatedQuestions)
+      })
+    
+  }
 
   return (
     <li>
@@ -15,9 +40,9 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
